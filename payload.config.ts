850206bc546433243d8573cb1a9fb5853bb3869c
maxim <bozzhik@gmail.com>
2025-10-01 16:34:15 +0300

    init `payload` for project `content` management
    https://payloadcms.com/docs/getting-started/installation#adding-to-an-existing-app

---
+++ payload.config.ts
@@ -0,0 +1,33 @@
+import {fileURLToPath} from 'url'
+import path from 'path'
+import sharp from 'sharp'
+
+import {lexicalEditor} from '@payloadcms/richtext-lexical'
+import {mongooseAdapter} from '@payloadcms/db-mongodb'
+import {buildConfig} from 'payload'
+
+import {Users} from '@payload/collections/Users'
+import {Media} from '@payload/collections/Media'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+
+  collections: [Users, Media],
+  editor: lexicalEditor(),
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  secret: process.env.PAYLOAD_SECRET || '',
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI || '',
+  }),
+  sharp,
+})
