52d83827734017e9173ff39d9335cb6c06d5e316
maxim <bozzhik@gmail.com>
2025-10-01 16:35:26 +0300

    create `member` payload collection

--- payload.config.ts
+++ payload.config.ts
@@ -8,6 +8,7 @@ import {buildConfig} from 'payload'
 
 import {Users} from '@payload/collections/Users'
 import {Media} from '@payload/collections/Media'
+import {Member} from '@payload/collections/Member'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -20,7 +21,7 @@ export default buildConfig({
     },
   },
 
-  collections: [Users, Media],
+  collections: [Users, Media, Member],
   editor: lexicalEditor(),
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
---
+++ src/payload/collections/Member.ts
@@ -0,0 +1,102 @@
+import type {CollectionConfig} from 'payload'
+
+export const Member: CollectionConfig = {
+  slug: 'member',
+  access: {
+    read: () => true,
+  },
+  admin: {
+    useAsTitle: 'name',
+  },
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'position',
+      type: 'select',
+      required: true,
+      hasMany: true,
+      options: [
+        {
+          label: 'Управляющий партнер',
+          value: 'partner',
+        },
+        {
+          label: 'Адвокат',
+          value: 'advocate',
+        },
+        {
+          label: 'Юрист',
+          value: 'lawyer',
+        },
+      ],
+    },
+    {
+      name: 'picture',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+    {
+      name: 'contacts',
+      type: 'group',
+      fields: [
+        {
+          name: 'phone',
+          type: 'number',
+          required: true,
+        },
+        {
+          name: 'email',
+          type: 'email',
+          required: true,
+        },
+      ],
+      required: true,
+    },
+    {
+      name: 'education',
+      type: 'array',
+      fields: [
+        {
+          name: 'degree',
+          type: 'text',
+          required: true,
+        },
+        {
+          name: 'period',
+          type: 'text',
+          required: true,
+        },
+      ],
+      required: true,
+    },
+    {
+      name: 'awards',
+      type: 'array',
+      fields: [
+        {
+          name: 'award',
+          type: 'text',
+          required: true,
+        },
+      ],
+      required: true,
+    },
+    {
+      name: 'about',
+      type: 'array',
+      fields: [
+        {
+          name: 'item',
+          type: 'text',
+          required: true,
+        },
+      ],
+      required: true,
+    },
+  ],
+}
